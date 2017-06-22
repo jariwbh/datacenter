ae700ddbbe3fba304476583542118cb0fe950995
samarth <samarth.magdallawala@krty.com>
2017-06-22 16:37:30 +0530

    Chnages

--- src/app/pages/users/components/add-user/add-user.component.ts
+++ src/app/pages/users/components/add-user/add-user.component.ts
@@ -11,20 +11,32 @@ export class AddUserComponent {
   informationVisibilty = true;
   usernamepasswordVisibilty = false;
   accesscontrolVisibilty = false;
+
+  _completedStep = 1;
   
   switchbox(value: any) {
     if (value === 'information') {
+      if (this._completedStep < 1) {
+        this._completedStep = 1;
+      }
       this.informationVisibilty = true;
       this.usernamepasswordVisibilty = false;
       this.accesscontrolVisibilty = false;
     } else if (value === 'usernamepassword') {
+      if (this._completedStep < 2) {
+        this._completedStep = 2;
+      }
       this.informationVisibilty = false;
       this.usernamepasswordVisibilty = true;
       this.accesscontrolVisibilty = false;
     } else if (value === 'controlaccess') {
+      if (this._completedStep < 3) {
+        this._completedStep = 3;
+      }
       this.informationVisibilty = false;
       this.usernamepasswordVisibilty = false;
       this.accesscontrolVisibilty = true;
     }
+    console.log(this._completedStep);
   }
 }
