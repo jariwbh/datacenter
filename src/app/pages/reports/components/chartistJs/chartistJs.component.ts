84e634f6dd6fd13f2f1285aaef490182c24804fe
prashant <krtyasoftwares@live.com>
2017-07-06 11:32:38 +0530

    Report design Update

--- src/app/pages/reports/components/chartistJs/chartistJs.component.ts
+++ src/app/pages/reports/components/chartistJs/chartistJs.component.ts
@@ -41,6 +41,17 @@ export class ChartistJsComponent {
   
   provinceList: any[] = [];
   districtList: any[] = [];
+
+  chartColors = {
+  red: 'rgb(255, 99, 132)',
+  orange: 'rgb(255, 159, 64)',
+  yellow: 'rgb(255, 205, 86)',
+  green: 'rgb(75, 192, 192)',
+  blue: 'rgb(54, 162, 235)',
+  purple: 'rgb(153, 102, 255)',
+  grey: 'rgb(231,233,237)',
+};
+
   constructor(private _chartistJsService: ChartistJsService, private _ReportService: ReportService) {
   }
 
