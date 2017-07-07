a279b93426f32e6bedbc05bd468359d393c2c9ad
prashant <krtyasoftwares@live.com>
2017-07-07 14:10:19 +0530

    Dashboard count update

--- src/app/pages/dashboard/trafficChart/trafficChart.service.ts
+++ src/app/pages/dashboard/trafficChart/trafficChart.service.ts
@@ -2,20 +2,27 @@ import { forEach } from '@angular/router/src/utils/collection';
 
 import { Injectable } from '@angular/core';
 import { BaThemeConfigProvider, colorHelper } from '../../../theme';
+import { SettingsService } from './../../../core/services/settings/settings.service';
 import { DashboardService } from './../../../core/services/dashboard/dashboard.service';
 
 @Injectable()
 export class TrafficChartService {
-   chartData: any[];
+  provinceCountList: any = [];
+  provinceCountSettingsList: any = [];
+  chartData: any[];
   // proChartData: any[];
   provinceData: any[];
-  proprovinceData: object[] = [];
-  constructor(private _baConfig: BaThemeConfigProvider, private _DashboardService: DashboardService) {
+  proprovinceData: any[] = [];
+  constructor(private _baConfig: BaThemeConfigProvider,
+    private _DashboardService: DashboardService,
+    private _Settings: SettingsService,
+  ) {
 
     // this.getAllChartData();
     this.getAllProvinceData();
+    this.getAllProvincePerData();
   }
- 
+
   // getAllChartData() {
   //   this._DashboardService.GetAllTrafficChartData().subscribe(data => {
 
@@ -27,24 +34,85 @@ export class TrafficChartService {
   // }
 
   getAllProvinceData() {
+
     this._DashboardService.GetAllProvinceInfo().subscribe(data => {
 
       if (data !== null) {
         this.provinceData = data;
-        this.provinceData.forEach( ele => {
+        this.provinceData.forEach(ele => {
           //console.log(ele);
-             this.proprovinceData.push({ value: 350,
-                color: ele.color, 
-               highlight: colorHelper.shade(ele.color, 15),
-                label: ele.name,
-                percentage: 30,
-                order: ele.order,
-              });
+          this.proprovinceData.push({
+            value: 350,
+            color: ele.color,
+            highlight: colorHelper.shade(ele.color, 15),
+            label: ele.name,
+            percentage: 0,
+            order: ele.order,
+            settingPersonCount: 0,
+            personCount: 0,
+          });
         });
         //console.log(this.proprovinceData);
       }
     });
-    
+
+  }
+
+  getAllProvincePerData() {
+    this._Settings.GetAllSetting().subscribe(data => {
+      if (data) {
+        if (data.noOfUserInProvince !== null) {
+          this.provinceCountSettingsList = data.noOfUserInProvince;
+          if (this.provinceCountSettingsList !== []) {
+            this._DashboardService.GetCountForProvince().subscribe(data1 => {
+              if (data1 !== null) {
+                //console.log( data1);
+                this.provinceCountList = data1;
+                if (this.provinceCountList !== []) {
+                  this.provinceCountList.forEach(ele => {
+                    //console.log( ele);
+                    this.proprovinceData.forEach(ele1 => {
+                      if (ele._id !== null) {
+                        // console.log( ele);
+                        // console.log( ele1);
+                        if (ele._id === ele1.label) {
+                          ele1.personCount = ele.count;
+                          let dBy = ele1.settingPersonCount;
+                          if (ele1.settingPersonCount === 0) {
+                            dBy = 1;
+                          }
+                          ele1.percentage = Math.floor(ele.count * 100 / dBy);
+
+                          this.provinceCountSettingsList.forEach(ele3 => {
+                            if (ele3.province === ele._id) {
+                              dBy = ele3.count;
+                              //console.log(dBy);
+                              //console.log(ele.count);
+                              // if (ele1.settingPersonCount === 0) {
+                              if (dBy === 0) {
+                                dBy = 1;
+                              }
+                              // ele1.percentage = Math.floor(ele1.personCount * 100 / dBy);
+                              ele1.percentage = Math.floor(ele.count * 100 / dBy);
+                              //console.log(ele1.percentage);
+                            }
+                          });
+
+
+                        }
+                      }
+                    });
+                  });
+                }
+              }
+            });
+
+
+          }
+        }
+      }
+    });
+
   }
 
   getData() {
--- src/app/pages/dashboard/usersMap/usersMap.component.ts
+++ src/app/pages/dashboard/usersMap/usersMap.component.ts
@@ -1,3 +1,5 @@
+import { forEach } from '@angular/router/src/utils/collection';
+import { DashboardService } from './../../../core/services/dashboard/dashboard.service';
 
 import { Router } from '@angular/router';
 import { layoutPaths } from './../../../theme/theme.constants';
@@ -13,36 +15,40 @@ import { CommonDataService } from './../../../core/services/common/common-data.s
   styleUrls: ['./usersMap.scss'],
 })
 export class UsersMap {
+  provinceCountList: any = [];
 
-  mapData: Object;
+  mapData: any;
 
-  constructor(private _usersMapService: UsersMapService, private _baConfig: BaThemeConfigProvider,
-   private _router: Router,
-   private _CommonDataService: CommonDataService) {
-     
-     let layoutColors = this._baConfig.get().colors;
+  constructor(private _usersMapService: UsersMapService,
+    private _baConfig: BaThemeConfigProvider,
+    private _router: Router,
+    private _CommonDataService: CommonDataService,
+    private _DashboardService: DashboardService,
+  ) {
 
-     this.mapData = {
+    let layoutColors = this._baConfig.get().colors;
+
+    this.mapData = {
       type: 'map',
       theme: 'blur',
       dataProvider: {
         map: 'iraqLow',
         //getAreasFromMap: true,
         areas: [
-          { title: 'Al-Basrah', id: 'IQ-BA', color: '#66ffb3', customData: '965', groupId: '1', custtitle: 'Basra' },
-          { title: 'Al-Qadisiyah', id: 'IQ-QA', color: '#66ff8c', customData: '685', groupId: '2', custtitle: 'Qadisiyah (Diwaniyah)' },
-          { title: 'Al-Muthannia', id: 'IQ-MU', color: '#66ffff', customData: '854', groupId: '3', custtitle: 'Muthanna' },
-          { title: 'An-Najaf', id: 'IQ-NA', color: '#66b3ff', customData: '754', groupId: '4', custtitle: 'Najaf' },
-          { title: 'Babylon', id: 'IQ-BB', color: '#0066cc', customData: '867', groupId: '5', custtitle: 'Babylon' },
-          { title: 'Baghdad', id: 'IQ-BG', color: '#0bb238', customData: '759', groupId: '6', custtitle: 'Baghdad' },
-          { title: 'Diyala', id: 'IQ-DI', color: '#ffb84d', customData: '302', groupId: '7', custtitle: 'Diyala' },
-          { title: 'Dhi-Qar', id: 'IQ-DQ', color: '#ffdaa1', customData: '102', groupId: '8', custtitle: 'Dhi Qar (Nasiriyah)' },
-          { title: 'Sala ad-Din', id: 'IQ-SD', color: '#ffd736', customData: '23', groupId: '9', custtitle: 'Salahaddin' },
-          { title: 'Karbala', id: 'IQ-KA', color: '#5eccc0', customData: '96', groupId: '10', custtitle: 'Karbala' },
-          { title: 'Kirkuk', id: 'IQ-KI', color: '#ffff77', customData: '80', groupId: '11', custtitle: 'Kirkuk' },
-          { title: 'Maysan', id: 'IQ-MA', color: '#997463', customData: '100', groupId: '12', custtitle: 'Maysan' },
-          { title: 'Ninawa', id: 'IQ-NI', color: '#55cc20', customData: '20', groupId: '13', custtitle: 'Ninewa (Mosul)' },
-          { title: 'Wasit', id: 'IQ-WA', color: '#cc5520', customData: '100', groupId: '14', custtitle: 'Wasit' },
+          { title: 'Al-Basrah', id: 'IQ-BA', color: '#66ffb3', customData: '0', groupId: '1', custtitle: 'Basra' },
+          { title: 'Al-Qadisiyah', id: 'IQ-QA', color: '#66ff8c', customData: '0', groupId: '2', custtitle: 'Qadisiyah (Diwaniyah)' },
+          { title: 'Al-Muthannia', id: 'IQ-MU', color: '#66ffff', customData: '0', groupId: '3', custtitle: 'Muthanna' },
+          { title: 'An-Najaf', id: 'IQ-NA', color: '#66b3ff', customData: '0', groupId: '4', custtitle: 'Najaf' },
+          { title: 'Babylon', id: 'IQ-BB', color: '#0066cc', customData: '0', groupId: '5', custtitle: 'Babylon' },
+          { title: 'Baghdad', id: 'IQ-BG', color: '#0bb238', customData: '0', groupId: '6', custtitle: 'Baghdad' },
+          { title: 'Diyala', id: 'IQ-DI', color: '#ffb84d', customData: '0', groupId: '7', custtitle: 'Diyala' },
+          { title: 'Dhi-Qar', id: 'IQ-DQ', color: '#ffdaa1', customData: '0', groupId: '8', custtitle: 'Dhi Qar (Nasiriyah)' },
+          { title: 'Sala ad-Din', id: 'IQ-SD', color: '#ffd736', customData: '0', groupId: '9', custtitle: 'Salahaddin' },
+          { title: 'Karbala', id: 'IQ-KA', color: '#5eccc0', customData: '0', groupId: '10', custtitle: 'Karbala' },
+          { title: 'Kirkuk', id: 'IQ-KI', color: '#ffff77', customData: '0', groupId: '11', custtitle: 'Kirkuk' },
+          { title: 'Maysan', id: 'IQ-MA', color: '#997463', customData: '0', groupId: '12', custtitle: 'Maysan' },
+          { title: 'Ninawa', id: 'IQ-NI', color: '#55cc20', customData: '0', groupId: '13', custtitle: 'Ninewa (Mosul)' },
+          { title: 'Wasit', id: 'IQ-WA', color: '#cc5520', customData: '0', groupId: '14', custtitle: 'Wasit' },
         ],
       },
       areasSettings: {
@@ -55,26 +61,26 @@ export class UsersMap {
       //smallMap: {},
       listeners: [{
         event: 'clickMapObject',
-        method: function(event) {
+        method: function (event) {
           //console.log(event.mapObject.custtitle);
-            _CommonDataService.filterDataBy = 'province';
-            _CommonDataService.filterData = event.mapObject.custtitle;
-            _router.navigate(['/pages/peoples/manage-people']);
+          _CommonDataService.filterDataBy = 'province';
+          _CommonDataService.filterData = event.mapObject.custtitle;
+          _router.navigate(['/pages/peoples/manage-people']);
         },
       }],
-     };
+    };
 
     //this.mapData = this._usersMapService.getData();
     //  this.mapData = {
     //   type: 'map',
     //   theme: 'blur',
-      
+
     //   zoomControl: { zoomControlEnabled: false, panControlEnabled: false },
 
     //   dataProvider: {
     //     // map: 'worldLow',
     //     map: 'iraqLow',
-        
+
     //     // zoomLevel: 3.5,
     //     // zoomLongitude: 10,
     //     // zoomLatitude: 52,
@@ -88,7 +94,7 @@ export class UsersMap {
     //     areas: [
     //       // { title: 'Austria', id: 'AT', color: layoutColors.primary, customData: '1 244', groupId: '1'},
     //       // { title: 'Ireland', id: 'IE', color: layoutColors.primary, customData: '1 342', groupId: '1'},
-   
+
     //       // { title: 'Hungary', id: 'HU', color: layoutColors.successLight, customData: '854', groupId: '2'},
     //       // { title: 'Cyprus', id: 'CY', color: layoutColors.successLight, customData: '754', groupId: '2'},
     //       // { title: 'Malta', id: 'MT', color: layoutColors.successLight, customData: '867', groupId: '2'},
@@ -129,47 +135,65 @@ export class UsersMap {
     //     unlistedAreasColor: layoutColors.defaultText,
     //     balloonText: '[[title]]: [[customData]] users',
     //   },
-      // legend: {
-      //   width: '100%',
-      //   marginRight: 27,
-      //   marginLeft: 27,
-      //   equalWidths: false,
-      //   backgroundAlpha: 0.3,
-      //   backgroundColor: layoutColors.border,
-      //   borderColor: layoutColors.border,
-      //   borderAlpha: 1,
-      //   top: 362,
-      //   left: 0,
-      //   horizontalGap: 10,
-      //   data: [
-      //     {
-      //       title: 'over 1 000 users',
-      //       color: layoutColors.primary,
-      //     },
-      //     {
-      //       title: '500 - 1 000 users',
-      //       color: layoutColors.successLight,
-      //     },
-      //     {
-      //       title: '100 - 500 users',
-      //       color: layoutColors.success,
-      //     },
-      //     {
-      //       title: '0 - 100 users',
-      //       color: layoutColors.danger,
-      //     },
-      //   ],
-      // },
-      // export: {
-      //   enabled: true,
-      // },
+    // legend: {
+    //   width: '100%',
+    //   marginRight: 27,
+    //   marginLeft: 27,
+    //   equalWidths: false,
+    //   backgroundAlpha: 0.3,
+    //   backgroundColor: layoutColors.border,
+    //   borderColor: layoutColors.border,
+    //   borderAlpha: 1,
+    //   top: 362,
+    //   left: 0,
+    //   horizontalGap: 10,
+    //   data: [
+    //     {
+    //       title: 'over 1 000 users',
+    //       color: layoutColors.primary,
+    //     },
+    //     {
+    //       title: '500 - 1 000 users',
+    //       color: layoutColors.successLight,
+    //     },
+    //     {
+    //       title: '100 - 500 users',
+    //       color: layoutColors.success,
+    //     },
+    //     {
+    //       title: '0 - 100 users',
+    //       color: layoutColors.danger,
+    //     },
+    //   ],
+    // },
+    // export: {
+    //   enabled: true,
+    // },
     //   creditsPosition: 'bottom-right',
     //   pathToImages: layoutPaths.images.amChart,
     // };
 
-
+    this.getAllProvinceData();
   }
 
+  getAllProvinceData() {
+    this._DashboardService.GetCountForProvince().subscribe(data1 => {
+      if (data1 !== null) {
+        this.provinceCountList = data1;
+        if (this.provinceCountList !== []) {
+          this.mapData.dataProvider.areas.forEach(ele => {
+            this.provinceCountList.forEach(ele1 => {
+              if (ele.custtitle === ele1._id) {
+                ele.customData = ele1.count;
+              }
+            });
+
+          });
+        }
+      }
+    });
+
+  }
 
 }
 
