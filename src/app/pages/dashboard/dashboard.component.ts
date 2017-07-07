ec6815773dbf33f76c323e9ec54f9ebab3df1a45
samarth <samarth.magdallawala@krty.com>
2017-07-07 18:01:44 +0530

    Dashboard changes

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -10,7 +10,7 @@ import { DashboardService } from './../../core/services/dashboard/dashboard.serv
 
 @Component({
   selector: 'dashboard',
-  styleUrls: ['./dashboard.scss'],
+  styleUrls: ['./dashboard.scss', './chartistJs.scss'],
   templateUrl: './dashboard.html',
 })
 export class Dashboard {
@@ -23,6 +23,12 @@ export class Dashboard {
 
  _topAdminlist: any[] = [];
 
+  allmonthNames: any[] = [];
+  data: any;
+  userMapHistory: any = {};
+
+  _chartLabelArray: any[] = [];
+
   constructor(
     private _router: Router,
     private _route: ActivatedRoute,
@@ -33,18 +39,97 @@ export class Dashboard {
   }
 
   ngOnInit() {
+    
+    this.allmonthNames = [
+        'January', 'February', 'March', 'April', 'May', 'June', 'July',
+        'August', 'September', 'October', 'November', 'December',
+        'January', 'February', 'March', 'April', 'May', 'June', 'July', 
+        'August', 'September', 'October', 'November', 'December',
+        ];
+
     //this.getAllAdmin();
     this.getAllFields('admin');
     this.getTopAdminData();
   }
 
   getTopAdminData() {
-     this._DashboardService.GetAllTopAdmin().subscribe( data => {
-          this._topAdminlist = data;
-          //console.log(this._topAdminlist);
+     this._DashboardService
+      .GetAllTopAdmin()
+      .subscribe( data => {
+          data.forEach(element => {
+            element.admin[0].admin.customCount = element.count;
+            element.admin[0].admin.custom_id = element._id;
+            this._topAdminlist.push(element.admin[0].admin);
+          });
      });
   }
 
+  rowClick(id: any) {
+    let isUser;
+    isUser = <HTMLInputElement> document.getElementById('row_' + id);
+    isUser.style.backgroundColor = 'rebeccapurple';
+    this.getMapBasedonAdmin(id);
+
+    this._topAdminlist.forEach(element => {
+      if (id !== element.custom_id) {
+         isUser = <HTMLInputElement> document.getElementById('row_' + element.custom_id);
+         isUser.style.backgroundColor = 'transparent';
+      }
+    });
+    
+  }
+  getMapBasedonAdmin(id: any) {
+    this._DashboardService
+      .GetAllTopAdminChart(id)
+      .subscribe( data => {
+        
+        // find Maximum Year
+        let maxYear = 0;
+        let maxMonth = 0;
+
+        data.forEach(element => {
+          if (element._id.year > maxYear ) {
+            maxYear = element._id.year;
+          }
+        });
+
+        if ( maxYear !== 0) {
+          data.forEach(element => {
+            if (element._id.year == maxYear ) {
+              if (element._id.month > maxMonth ) {
+                maxMonth = element._id.month;
+              }
+            }
+          });
+        }
+        let cnt = 1;
+        let anotherCounter = 1;
+        this.allmonthNames.forEach(element => {
+          if (cnt >= maxMonth ) {
+            if (anotherCounter <= 12) {
+              //console.log(cnt);
+              this._chartLabelArray.push(element);
+              anotherCounter++;
+            }
+          }
+          cnt++;
+        });
+        // console.log(this._chartLabelArray);
+        // console.log( maxYear + ' --> '  + maxMonth );
+        // console.log(this.allmonthNames[parseInt(maxMonth) + 1]);
+        this.userMapHistory = {
+              labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+              series: [
+                [15, 24, 43, 27, 5, 10, 23, 44, 68, 100, 26, 8],
+              ],
+              simpleBarOptions: {
+                fullWidth: true,
+                height: '300px',
+              },
+        };
+
+      });
+  }
   getAllFields(id: any) {
     this._fieldsService
         .GetAll(id)
