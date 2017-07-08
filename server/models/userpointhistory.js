368c71ce18152128fcd76851703c62ec450f3cd7
jariwbh@krtya.com <jariwbh@gmail.com>
2017-07-08 12:21:37 +0530

    model updated

--- server/models/userpointhistory.js
+++ server/models/userpointhistory.js
@@ -6,6 +6,8 @@ var Schema       = mongoose.Schema;
 var PersonPointHistorySchema   = new Schema({
     personid: {type: mongoose.Schema.Types.ObjectId, ref: 'person'},
     point: Number,
+    province: String,
+    district: String,
     activity: String,
     date: Date
 });
